36a5c08382e15c25d29fe13fee4bba3e034690bf
MartinPerezDev <martinperez9713@gmail.com>
2025-12-16 21:10:02 -0300

    unidad-8: conectado nuestra app con mongo atlas

---
+++ unidad-8/src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import productsRouter from "./routes/products.router.js";
+import connectMongoDB from "./config/db.js";
+import dotenv from "dotenv";
+import __dirname from "../dirname.js";
+import { errorHandler } from "./middlewares/error.middleware.js";
+
+//incializamos las variables de entorno
+dotenv.config({ path: __dirname + "/.env" });
+
+const app = express();
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+const PORT = process.env.PORT || 8080 ;
+
+connectMongoDB();
+
+//endpoints
+app.use("/api/products", productsRouter);
+
+
+app.use(errorHandler);
+
+app.listen(PORT, ()=> {
+  console.log("Servidor iniciado correctamente!");
+});
---
+++ unidad-8/src/routes/products.router.js
@@ -0,0 +1,15 @@
+import express from "express";
+import Product from "../models/product.model.js";
+import { getAllProducts, addProduct, setProductById, deleteProductById } from "../controllers/products.controller.js"
+
+const productsRouter = express.Router();
+
+productsRouter.get("/", getAllProducts);
+
+productsRouter.post("/", addProduct);
+
+productsRouter.put("/:pid", setProductById);
+
+productsRouter.delete("/:pid", deleteProductById);
+
+export default productsRouter;
